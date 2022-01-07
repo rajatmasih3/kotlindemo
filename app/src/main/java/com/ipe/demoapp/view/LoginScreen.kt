cb0e83cdcc5fece6986073d414f0621c3b33fbc4
Rajat Masih <rajat.masih3@gmail.com>
2022-01-07 19:38:07 +0530

    Login Screen added

---
+++ app/src/main/java/com/ipe/demoapp/view/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.ipe.demoapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ipe.demoapp.R
+
+class LoginScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
